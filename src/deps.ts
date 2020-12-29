63242c8653d14757b0df203834a40d7859ba5baa
Trevor D. Brown <trevordylanbrown@gmail.com>
2020-12-28 23:52:44 -0600

    Create deps.ts
    - Referenced resources to assist with application development and implementation: WebView and Path.

---
+++ src/deps.ts
@@ -0,0 +1,16 @@
+/*
+    Texpansion
+    (c)2020 Trevor D. Brown. All rights reserved.
+
+    deps.ts - the dependencies for the entire project.
+
+*/
+
+// WebView - allows the creation of windowed applications rendered with web pages.
+export {
+    Webview
+} from "https://deno.land/x/webview@0.5.5/mod.ts";
+
+export
+    * 
+as path from "https://deno.land/std@0.82.0/path/mod.ts";
