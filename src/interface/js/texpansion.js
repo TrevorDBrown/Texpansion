acb1a46c281fbbb8bcd50b5c16d8eb7a217a7fb2
Trevor D. Brown <trevordylanbrown@gmail.com>
2020-12-28 23:49:52 -0600

    Create texpansion.js
    - Initialized core JavaScript file for UI logic.

---
+++ src/interface/js/texpansion.js
@@ -0,0 +1,11 @@
+/*
+    Texpansion
+    (c)2020 Trevor D. Brown. All rights reserved.
+
+    js/texpansion.js - primary logic for application's UI.
+
+*/
+
+$(() => {
+    console.log("Hello!");
+});
